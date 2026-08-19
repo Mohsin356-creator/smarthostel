036b49da9eee25accc9f30c4e40efbdda4833c25
Mohsin356-creator <mohsinrais210@gmail.com>
2026-08-19 14:07:44 +0500

    Prepare for deployment

--- hostelproject/settings.py
+++ hostelproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dh!@1z8*2x^2dfjy+8)rqy50by2-2&j%13s)n23gxhbj+km74)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,3 +131,6 @@ MAILERS = {
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
